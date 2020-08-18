bb210da656effa563ad7833e012b0d3d47add981
James Lees <jamestrevorlees@gmail.com>
2020-08-18 06:07:47 +0200

    create variables to grab the elements of the UI we need for the calculation and output

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 // Listen for submit
 
-document.getElementById('loan-form').addEventListener('submit',calculateResults);
+document
+  .getElementById("loan-form")
+  .addEventListener("submit", calculateResults);
 
+// Calculate Results
+
+function calculateResults(e) {
+  console.log("Calculating...");
+
+  //grab everything we need from the UI
+
+  // UI Variables
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+  
+
+
+
+
+  // since it is a form submit we will prevent defaults
+  e.preventDefault();
+}
